0bf9a6912d769310963965953725928c27bc8265
jokemanfire <981376433@qq.com>
2022-03-23 21:35:42 +0800

    增加超级白嫖算法

--- CodeCraft-202201.cpp
+++ CodeCraft-202201.cpp
@@ -4,25 +4,21 @@
 #include<string>
 #include<vector>
 #include<sstream>
-#include<map>
 #include<set>
 #include<algorithm>
-#include<numeric>
 #include<unordered_map>
-#include<thread>
-#include<climits>
+
 using namespace std;
 
-
 //作品提交路径
-string file_root = "/data/";
+string file_root = "data/";
 string file_Demand = "demand.csv";
 string file_Bandwidth = "site_bandwidth.csv";
 string file_qos = "qos.csv";
 string file_config = "config.ini";
-string file_output = "/output/solution.txt";
+string file_output = "solution.txt";
 
-int ALLMax_times = 1;
+
 void InitAverCount(vector<vector<int>>& CountJu);
 
 //一般数据格式内容
@@ -65,29 +61,6 @@ public:
 
 };
 
-struct MinHeapCmp
-{
-	inline bool operator()(map<int, int>& x, map<int, int>& y)
-	{
-		return x.begin()->second > y.begin()->second;
-	}
-};
-
-class MyHeap
-{
-private:
-	vector<map<int, int>> Indexs;
-	int max_size = 5;
-	map<int, int> Drop_index;
-public:
-	MyHeap() { make_heap(Indexs.begin(), Indexs.end(), MinHeapCmp()); };
-	bool Pushdata(map<int, int> data);
-	map<int, int> GetDrop_index() { return Drop_index; };
-	vector<map<int, int>>& GetIndex() {
-		return Indexs;
-	};
-	void SetMax_size(int size) { max_size = size; };
-};
 
 class UserManage
 {
@@ -114,8 +87,8 @@ private:
 	unordered_map<string, vector<bool>> UsefulUser_flag;
 	//存储node的带宽
 	unordered_map<string, int> Node_Width;
-	//存储时刻下最大使用节点
-	vector<vector<int>>Time_node;
+
+	
 public:
 	vector<string>& Get_Nodenames() { return Nodenames; };
 	void AddNode_Usefuluser(string nodename, bool flag) { UsefulUser_flag[nodename].push_back(flag); };
@@ -125,57 +98,14 @@ public:
 		return Node_Width[name];
 	}
 	void PushName(string name) { Nodenames.push_back(name); };
-	vector<vector<int>>& GetTimeNode() {
-		return Time_node;
-	};
+	//存储所有节点时间开销
+	vector<vector<int>> Node_Pay;
 	//存储优先分配节点次数
 	vector<int> Node_count;
-	//存储分配次数
-	vector<float> Node_float;
-	vector<vector<int>>Cost_time;
-	vector<MyHeap*>Node_heap;
-	vector < vector < bool>> UnconNodes;
+	vector<vector<int>> Time_node;
 };
 
 
-
-bool MyHeap::Pushdata(map<int, int> data)
-{
-	if (Indexs.size() == max_size)
-	{
-		/*sort_heap(Indexs.begin(), Indexs.end(), MinHeapCmp());*/
-		map<int, int> First = Indexs[0];
-		//如果传入数据比堆数据小
-		if (data.begin()->second <= First.begin()->second)
-		{
-			Drop_index = data;
-			//make_heap(Indexs.begin(), Indexs.end());
-			return false;
-		}
-		else
-		{
-			pop_heap(Indexs.begin(), Indexs.end());
-			map<int, int> temp = Indexs.back();
-			Indexs.pop_back();
-			Indexs.push_back(data);
-			push_heap(Indexs.begin(), Indexs.end(), MinHeapCmp());
-			Drop_index = temp;
-			//make_heap(Indexs.begin(), Indexs.end());
-			return false;
-		}
-	}
-	else
-	{
-		//如果堆的大小比最大小 直接放入
-		if (Indexs.size() < max_size)
-		{
-			Indexs.push_back(data);
-			push_heap(Indexs.begin(), Indexs.end(), MinHeapCmp());
-		}
-		return true;
-
-	}
-}
 
 //字符串切割方法
 std::vector<std::string> stringSplit(const std::string& str, char delim) {
@@ -230,7 +160,11 @@ DatasStruct GetData(string filepath)
 	return tempData;
 }
 
-
+int ChooseNode(vector<int>& DD)
+{
+	int Index = distance( DD.begin(),min_element(DD.begin(), DD.end()));
+	return Index + 1;
+}
 
 //读取ini文件中的配置
 int ReadQos(string filepath)
@@ -246,47 +180,8 @@ int ReadQos(string filepath)
 	return atoi(Result.c_str());
 }
 
-//超级贪心算法 user对目标用户分配，node 对目标节点分配
-int ChooseNode(vector<vector<int>>& allc,int user, vector<bool> &UnconNode)
-{
-	vector<int> allNodecount(allc[0].size()-1, 0);
-	int index = -1;
-	for (int i = 1; i < allc[0].size(); i++)
-	{
-		int NodeWidt = 0;
-		if (allc[user][i] == -1 || allc[0][i] == 0)
-		{
-			allNodecount[i - 1] = -1;
-			continue;
-		}
-			
-		for (int j = 1; j < allc.size(); j++)
-		{
-			int this_need = allc[j][0];
-			if(this_need != -1  )
-			NodeWidt += this_need;
-		}
-		allNodecount[i - 1] = abs(allc[0][i] - NodeWidt);
-	}
-
-	//获取最小坐标
-	for (int i = 0; i < allNodecount.size(); i++)
-	{
-		if (UnconNode[i] == true || allNodecount[i] == -1)
-			continue;
-		if (index != -1 && allNodecount[i] < allNodecount[index])
-			index = i;
-		else if (index == -1)
-			index = i;
-	}
-	//如果实在没有选择
-	if (index == -1)
-		return -1;
-	return index + 1;
-}
-
 //针对一个用户的分配
-void NodeAssign(vector<vector<int>>& allc, int user, int node)
+void NodeAssign(vector<vector<int>>& allc, int user, int node, bool av)
 {
 	int j = node;
 	// if(av)
@@ -295,9 +190,7 @@ void NodeAssign(vector<vector<int>>& allc, int user, int node)
 	int Count = 0;
 	while (Count < Max_Count)
 	{
-		//每次选择占比最大节点
-		int s;
-		s = (j + Count) % ((allc[user].size() - 1) + 1);
+		int s = (j + Count) % ((allc[user].size() - 1) + 1);
 		//如果不能分配
 		if (allc[user][s] == -1)
 		{
@@ -338,10 +231,10 @@ void NodeAssign(vector<vector<int>>& allc, int user, int node)
 	}
 }
 //重新分配
-void Reset(vector<vector<int>>& allc, int user, int node)
+void Reset(vector<vector<int>>& allc, int user, int node, bool av)
 {
 	//减少分配
-	int Rest_width = allc[user][0];
+	int &Rest_width = allc[user][0];
 	int i = node;
 	// if(av)
 	// 	i = rand()% (user-1)+1;
@@ -372,37 +265,36 @@ void Reset(vector<vector<int>>& allc, int user, int node)
 		Count++;
 	}
 }
-
-void AverageChoose(vector<vector<int>>& allc, int user, int node)
+//迭代分配 贪心算法 user对目标用户分配，node 对目标节点分配
+void AverageChoose(vector<vector<int>>& allc, int user, int node, bool av,vector<int> & NodeCount)
 {
 	if (user < allc.size())
 	{
-		
-		NodeAssign(allc, user, node);
+		NodeAssign(allc, user, node, av);
 	}
 	else
 		return;
 	//如果没有分配完
 	if (allc[user][0] != 0)
 	{
-		for (int j = 1; j < allc[user].size(); j++)
+		for (int j = node; j < allc[user].size(); j++)
 		{
 			if (allc[user][j] >= 0)
 			{
 				//Reset后重新分配
-				Reset(allc, user, j);
+				Reset(allc, user, j, av);
 				//对该用户分配
-				AverageChoose(allc, user, j);
+				AverageChoose(allc, user, j, av, NodeCount);
+				if (allc[user][0] == 0)
+					break;
 			}
 		}
-	
-
 		return;
 	}
 	//分配完继续下一个
 	else
 	{
-		AverageChoose(allc, user + 1, 1);
+		AverageChoose(allc, user + 1, 1, av, NodeCount);
 		return;
 	}
 }
@@ -416,7 +308,7 @@ int IsEffect(vector<vector<int>>& CountJu)
 	}
 	return -1;
 }
-
+//初始化均值
 void InitAverCount(vector<vector<int>>& CountJu)
 {
 	for (int i = 1; i < CountJu.size(); i++)
@@ -441,46 +333,15 @@ void InitAverCount(vector<vector<int>>& CountJu)
 		}
 	}
 }
-
-void InitMaxData(vector<vector<int>>& CountJu, int node)
-{
-	for (int i = 1; i < CountJu.size(); i++)
-	{
-		//如果节点可分配带宽大于0 && 节点为联通
-		if (CountJu[0][node] > 0 && CountJu[i][node] != -1)
-		{
-
-			//如果能全分配
-			if (CountJu[0][node] >= CountJu[i][0])
-			{
-				CountJu[0][node] -= CountJu[i][0];
-				CountJu[i][node] += CountJu[i][0];
-				CountJu[i][0] -= CountJu[i][0];
-			}
-			//不能全分配
-			else
-			{
-				CountJu[i][0] -= CountJu[0][node]; //该用户
-				CountJu[i][node] += CountJu[0][node]; //该用户的该节点
-				CountJu[0][node] -= CountJu[0][node]; //该节点
-			}
-		}
-	}
-}
-
 //对节点最大值初始化
-bool InitMaxCount(vector<vector<int>>& CountJu, vector<bool>& UnconNode)
+void InitMaxCount(vector<vector<int>>& CountJu,int node)
 {
 	for (int i = 1; i < CountJu.size(); i++)
 	{
-		int node = ChooseNode(CountJu, i, UnconNode);
-		//该用户没有适配节点跳过
-		if (node == -1)
-			continue;
 		//如果节点可分配带宽大于0 && 节点为联通
 		if (CountJu[0][node] > 0 && CountJu[i][node] != -1)
 		{
-
+			
 			//如果能全分配
 			if (CountJu[0][node] >= CountJu[i][0])
 			{
@@ -496,18 +357,36 @@ bool InitMaxCount(vector<vector<int>>& CountJu, vector<bool>& UnconNode)
 				CountJu[0][node] -= CountJu[0][node]; //该节点
 			}
 		}
+		
 	}
-	return true;
 }
-
+//单次计算开销 
+int JudgePay(vector<vector<int>>& Time_Paid)
+{
+	static int Paid = 0;
+	for (int i = 0; i < Time_Paid[0].size(); i++)
+	{
+		vector<int> This_node;
+		for (int j = 0; j < Time_Paid.size(); j++)
+		{
+			This_node.push_back(Time_Paid[j][i]);
+		}
+		sort(This_node.begin(), This_node.end());
+		Paid += This_node[(This_node.size() * 0.96)];
+	}
+	return Paid;
+}
 //时间节点上的分配 
-void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool av, ofstream& outfile, int node_number = 1)
+void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool av, ofstream& outfile)
 {
 	//初始化计算矩阵
 	vector<string>Usernames = Um->Get_usersnames();
 	vector<string>Nodenames = Nm->Get_Nodenames();
 	vector<vector<int>> CountJu(Usernames.size() + 1, vector<int>(Nodenames.size() + 1));
+	//行头 用户所需带宽
+	//列头 Node所拥有的带宽
 
+	static int node_number = 1;
 
 	//初始化矩阵分配数值 不能分配为-1
 	for (int i = 0; i < Nodenames.size(); i++)
@@ -522,6 +401,12 @@ void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool
 		}
 	}
 
+
+	//初始化分配次数
+	for (int i = 0; i < Nodenames.size(); i++)
+	{
+		Nm->Node_count.push_back(0);
+	}
 	for (int i = Min_time; i < Max_time; i++)
 	{
 		vector<vector<int>> CountJuTemp = CountJu;
@@ -529,6 +414,7 @@ void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool
 
 		for (int k = 1; k < Usernames.size() + 1; k++)
 		{
+
 			//列头
 			CountJuTemp[k][0] = Um->GetWidth(i, Usernames[k - 1]);
 		}
@@ -538,17 +424,34 @@ void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool
 			CountJuTemp[0][j] = Nm->GetWidth(Nodenames[j - 1]);
 		}
 		if (av)
-		{
 			InitAverCount(CountJuTemp);
-		}
 		else
-			InitMaxData(CountJuTemp, node_number);
+			InitMaxCount(CountJuTemp, node_number);
 		int Reuslt = 1;
+
+
+
+	
 		while (Reuslt != -1)
 		{
-			AverageChoose(CountJuTemp, Reuslt, node_number);
+			AverageChoose(CountJuTemp, Reuslt, node_number, av, Nm->Node_count);
+			// if(!av)
+			// {
+			if ((node_number + 1) % (CountJuTemp[0].size()) == 0)
+				node_number = 1;
+			else
+				node_number = (node_number + 1) % (CountJuTemp[0].size());
+			// }
 			Reuslt = IsEffect(CountJuTemp);
 		}
+
+		//将开销放入数组
+		for (int k = 1; k < CountJuTemp.size(); k++)
+		{
+			int width = CountJuTemp[0][k];
+			Nm->Node_Pay[i][k - 1] = Nm->GetWidth(Nodenames[k - 1]) - width;
+		}
+
 		//获得分配结果
 		for (int i = 1; i < CountJuTemp.size(); i++)
 		{
@@ -565,140 +468,6 @@ void DealOneAlg(int Min_time, int Max_time, UserManage* Um, NodeManage* Nm, bool
 			outfile << endl;
 		}
 	}
-	return;
-}
-
-int GetNode(vector<vector<int>>& allc, int user, vector<float>& NodeCount)
-{
-	vector<int> allNodecount(allc[0].size() - 1, 0);
-	int index = 0;
-	for (int i = 1; i < allc[0].size(); i++)
-	{
-		int NodeWidt = 0;
-		if (allc[user][i] == -1 || allc[0][i] == 0)
-		{
-			allNodecount[i - 1] = -1;
-			continue;
-		}
-
-		for (int j = 1; j < allc.size(); j++)
-		{
-			int this_need = allc[j][0];
-			if (this_need != -1)
-				NodeWidt += this_need;
-		}
-		allNodecount[i - 1] = abs(allc[0][i] - NodeWidt);
-		index = i - 1;
-	}
-
-	//获取最小坐标
-	for (int i = 0; i < allNodecount.size(); i++)
-	{
-
-		if (allNodecount[i] >= 0 && allNodecount[i] < allNodecount[index])
-			index = i;
-	}
-	return index + 1;
-}
-
-void InitMax(int time, UserManage* Um, NodeManage* Nm)
-{
-	
-	vector<string>Usernames = Um->Get_usersnames();
-	vector<string>Nodenames = Nm->Get_Nodenames();
-	vector<vector<int>> CountJu(Usernames.size() + 1, vector<int>(Nodenames.size() + 1));
-	//行头 用户所需带宽
-	//列头 Node所拥有的带宽
-
-
-
-	//初始化矩阵分配数值 不能分配为-1
-	for (int i = 0; i < Nodenames.size(); i++)
-	{
-		vector<bool> flags = Nm->Get_UsefulUser(Nodenames[i]);
-		for (int j = 0; j < flags.size(); j++)
-		{
-			if (flags[j])
-				CountJu[j + 1][i + 1] = 0;
-			else //不能分配节点置为-1
-				CountJu[j + 1][i + 1] = -1;
-		}
-	}
-
-
-
-	//获取当前时刻下的映射
-
-	for (int k = 1; k < Usernames.size() + 1; k++)
-	{
-		//列头
-		CountJu[k][0] = Um->GetWidth(time, Usernames[k - 1]);
-	}
-	for (int j = 1; j < Nodenames.size() + 1; j++)
-	{
-		//行头
-		CountJu[0][j] = Nm->GetWidth(Nodenames[j - 1]);
-	}
-	
-	int Reuslt = 1;
-	/*while (Reuslt != -1)
-	{
-		AverageChoose(CountJu, Reuslt, 1);
-		Reuslt = IsEffect(CountJu);
-	}*/
-	InitMaxCount(CountJu, Nm->UnconNodes[time]);
-	
-	//取消耗最大
-	
-	vector<int> Customers;
-	Customers.resize(CountJu[0].size() - 1);
-	for (int i = 1; i < CountJu[0].size(); i++)
-	{
-		int UsedWidth = Nm->GetWidth(Nodenames[i - 1]) - CountJu[0][i];
-		Customers[i - 1] = UsedWidth;
-	}
-	vector<int>::iterator max_index =max_element(Customers.begin(), Customers.end());
-
-	int Max_index =  std::distance(Customers.begin(), max_index);
-	if (Customers[Max_index] != 0)
-	{
-		map<int, int> temp;
-		temp[time] = Customers[Max_index];
-		bool status = Nm->Node_heap[Max_index]->Pushdata(temp);
-		//没能放入堆里 
-		if (!status)
-		{
-
-			//针对该时间节点重新分配
-			map<int, int> tm = Nm->Node_heap[Max_index]->GetDrop_index();
-			//cout << "time " << tm.begin()->first << " 节点" << Nodenames[Max_index] << endl;
-			Nm->UnconNodes[tm.begin()->first][Max_index] = true;
-			InitMax(tm.begin()->first, Um, Nm);
-
-		}
-	}
-	//全局最大 时间复杂度太高
-	//for (int i = 1; i < CountJu[0].size(); i++)
-	//{
-
-	//	int UsedWidth = Nm->GetWidth(Nodenames[i - 1]) - CountJu[0][i];
-	//	if (UsedWidth != 0)
-	//	{
-	//		map<int, int> temp;
-	//		temp[time] = UsedWidth;
-	//		bool status = Nm->Node_heap[i - 1]->Pushdata(temp);
-	//		//没能放入堆里 
-	//		if (!status)
-	//		{
-	//			//针对该时间节点重新分配
-	//			map<int, int> tm = Nm->Node_heap[i - 1]->GetDrop_index();
-	//			Nm->UnconNodes[tm.begin()->first][i-1] = true;
-	//			InitMax(tm.begin()->first, Um, Nm);				
-	//		}
-	//	}
-	//}
-	
-
 	return;
 }
 
@@ -740,7 +509,7 @@ int main()
 		int NodeWidth = atoi(NodeWidths.Datas[i][1].c_str());
 		Nm->SetWidth(Nodename, NodeWidth);
 	}
-	ALLMax_times = Max_times;
+
 
 	//初始化Node可用用户
 	for (int i = 0; i < PeopleQos.Datas.size(); i++)
@@ -773,66 +542,33 @@ int main()
 		}
 	}
 
-	vector<vector<int>>& Time_node = Nm->GetTimeNode();
-	Time_node.resize(Max_times);
-
-
-	ofstream outfile(file_output);
-	Nm->Cost_time.resize(Nm->Get_Nodenames().size());
-	for (int i = 0; i < Nm->Cost_time.size(); i++)
-		Nm->Cost_time[i].resize(Max_times);
-	
-
-
-	//求所有节点最爽的时间点
-	for (int i = 0; i < Nm->Get_Nodenames().size(); i++)
-	{
-		MyHeap * new_heap = new MyHeap;
-		int threld = Max_times * 0.05;
-		if ((Max_times * 100) % 5 == 0)
-			threld -= 1;
-		new_heap->SetMax_size(threld);
-		Nm->Node_heap.push_back(new_heap);
-	}
-
-	
-
-	//获得时间下最适配带宽
-	Nm->UnconNodes.resize(Max_times);
+	//初始化不同时刻开销
+	Nm->Node_Pay.resize(Max_times);
 	for (int i = 0; i < Max_times; i++)
 	{
-		Nm->UnconNodes[i].resize(Nm->Get_Nodenames().size());
+		Nm->Node_Pay[i].resize(Nm->Get_Nodenames().size());
 	}
-	for (int time = 0; time < Max_times; time++)
+
+	ofstream outfile(file_output);
+
+	int Min_time = 0;
+	int n_time = Max_times * 0.05;
+	DealOneAlg(Min_time, Max_times, Um, Nm, false, outfile);
+	//DealOneAlg(n_time, Max_times, Um, Nm, true, outfile);
+
+
+
+
+	//获得开销
+	cout<<"score:" <<JudgePay(Nm->Node_Pay);
+	ofstream Kfile("pay_data.txt");
+	for (int i = 0; i < Nm->Node_Pay.size(); i++)
 	{
-		InitMax(time, Um, Nm);
-	
-	}
-	map<int, int> TimeNode;
-	for (int i = 0; i < Nm->Node_heap.size(); i++)
-	{
-		vector<map<int, int>>& temp = Nm->Node_heap[i]->GetIndex();
-		for (int j = 0; j < temp.size(); j++)
+		for (int j = 0; j < Nm->Node_Pay[i].size(); j++)
 		{
-			int time = temp[j].begin()->first;
-			TimeNode[time] = i;
+			Kfile << "  Nodename:" << Nm->Get_Nodenames()[j] << "  " << Nm->Node_Pay[i][j];
 		}
-		
+		Kfile << endl;
 	}
-
-	for (int time = 0; time < Max_times; time++)
-	{
-		int node = 0;
-		if (TimeNode.count(time) != 0)
-		{
-			node = TimeNode[time];
-			DealOneAlg(time, time + 1, Um, Nm, false, outfile, node + 1);
-		}
-		else
-			DealOneAlg(time, time + 1, Um, Nm, true, outfile, node + 1);
-	}
-
-
-
 	return 0;
 }
